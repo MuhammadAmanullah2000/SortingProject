5f368c1432c450f21f282d881341ed631465e5c2
Muhammad <chelseafc56057@gmail.com>
2022-04-13 09:47:59 +0500

    oddEven

--- custom-sorts.js
+++ custom-sorts.js
@@ -3,13 +3,13 @@ function ageSort(users) {
   let sortedUsers = [];
 
 
-  while(copyUsers.length > 0) {
+  while (copyUsers.length > 0) {
     let index;
     let lowestAge = Infinity;
 
-    for(let i = 0; i < copyUsers.length; i++) {
+    for (let i = 0; i < copyUsers.length; i++) {
       let current = copyUsers[i]
-      if(current["age"] < lowestAge) {
+      if (current["age"] < lowestAge) {
         lowestAge = current["age"]
         index = i;
       }
@@ -26,6 +26,35 @@ function ageSort(users) {
 
 function oddEvenSort(arr) {
   // Your code here
+  let even = [];
+  let odd = [];
+  while (arr.length > 0) {
+    let smallestEven = Infinity;
+    let smallestOdd = Infinity;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        if(arr[i]<smallestEven){
+          smallestEven = arr[i];
+        }
+      } else {
+        if(arr[i]<smallestOdd){
+          smallestOdd = arr[i];
+        }
+      }
+    }
+    if(smallestEven !== Infinity){
+      even.push(smallestEven);
+      let smallestEvenIndex = arr.indexOf(smallestEven);
+      arr.splice(smallestEvenIndex,1);
+    }
+    if(smallestOdd !== Infinity){
+      odd.push(smallestOdd);
+      let smallestOddIndex = arr.indexOf(smallestOdd);
+      arr.splice(smallestOddIndex,1);
+    }
+
+  }
+  return odd.concat(even);
 }
 
 function validAnagrams(s, t) {
